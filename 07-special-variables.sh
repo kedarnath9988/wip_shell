aa83d6756e058aabcdd6790e91d2008e23c86452
kedarnath9988 <anubolukedarnath9988@gmail.com>
2025-06-12 09:15:43 +0530

    basic-files

--- 07-special-variables.sh
+++ 07-special-variables.sh
@@ -4,6 +4,8 @@ echo "to known all variables passed in in CLI "$@
 
 echo "to count "$#
 
+echo " current shell script name is " $0 
+
 echo " to know current user is" $USER
 
 echo "current work-dir" $PWD 

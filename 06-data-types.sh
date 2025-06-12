20fb5041eb422bbe1f7e740c059baf1e247dfe97
kedarnath9988 <anubolukedarnath9988@gmail.com>
2025-06-12 09:01:59 +0530

    basic-files

---
+++ 06-data-types.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+num=10
+num2=20
+
+echo " sum of 2 nums is "$((num + num2))
+echo "sub of 2 nums is "$((num2 - num ))
+echo " product od 2 nums is "$((num * num2 ))
+echo "div of 2 nums is " $(( num2 % num ))

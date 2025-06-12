ce5f3fcaa9e11ffd1551438c6959ddc28d67e667
kedarnath9988 <anubolukedarnath9988@gmail.com>
2025-06-12 08:47:32 +0530

    basic-files

---
+++ 03-declear-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+person1="kedarnath"
+person2="naveen"
+echo "person1: Heelo $person2"
+echo "$person2: hello $person1"
+echo "$person1 : how are you $person2"
+echo "$person2: yea ..! good about you $person1" 
